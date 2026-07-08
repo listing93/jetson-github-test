fb5602338336530c10ae3a87f398b4f50a381f98
listing <ellalee0314@gmail.com>
2026-07-08 13:21:43 +0800

    initial test

---
+++ test.py
@@ -0,0 +1,7 @@
+import platform
+from datetime import datetime
+
+print("Hello!")
+print("Current date and time:", datetime.now())
+print("Operating System:", platform.system())
+print("Machine:", platform.machine())
